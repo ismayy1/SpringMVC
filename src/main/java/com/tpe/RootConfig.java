d20ca03a32b0f9e32571589cd671bde734ee8f38
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-16 19:45:25 +0300

    Creating Spring MVC app, setting dependencies, creating postgreSQL database

--- src/main/java/com/tpe/RootConfig.java
+++ src/main/java/com/tpe/RootConfig.java
@@ -1,4 +1,27 @@
 package com.tpe;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@PropertySource("classpath:db.properties")
 public class RootConfig {
+
+    @Autowired
+    private Environment environment;
+
+    // JDBC and Hibernate Configurations
+    @Bean
+    public LocalSessionFactoryBean sessionFactory () {
+
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+
+        return sessionFactory;
+    }
+
 }
