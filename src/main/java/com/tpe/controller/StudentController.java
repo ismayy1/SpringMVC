93d6850bcbb58092f8b1d42d8f9dbff5bd214e6d
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-16 20:50:38 +0300

    Creating Spring MVC app, setting dependencies, creating postgreSQL database

--- src/main/java/com/tpe/controller/StudentController.java
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.tpe.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,6 +11,8 @@ import org.springframework.web.servlet.ModelAndView;
 // we can only use @RequestMapping in method level, then the path will be valid for method only
 public class StudentController {
 
+    // http://localhost:8080/springMvc/students/hi + GET  => end point
+    @GetMapping ("/hi") // HTTP method type
     public ModelAndView sayHi () {
         ModelAndView mav = new ModelAndView();
         mav.addObject("message", "Hello");
