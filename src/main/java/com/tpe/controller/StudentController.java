47c92dc50ba9d571f55ab8dc4bf0293113ff5180
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-16 20:45:05 +0300

    Creating Spring MVC app, setting dependencies, creating postgreSQL database

--- src/main/java/com/tpe/controller/StudentController.java
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -2,10 +2,19 @@ package com.tpe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller // to handle requests
+@Controller // special component to handle requests
 @RequestMapping ("/students") // http://localhost:8080/springMvc/students  => end point should be directed to this requestMapping
 // we can only use @RequestMapping in class level. if it's used in class level, it's valid for all methods
 // we can only use @RequestMapping in method level, then the path will be valid for method only
 public class StudentController {
+
+    public ModelAndView sayHi () {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("message", "Hello");
+        mav.addObject("messagebody", "I am a Student Management System");
+        return mav;
+    }
+
 }
