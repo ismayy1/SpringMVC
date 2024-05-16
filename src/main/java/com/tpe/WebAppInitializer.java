bf0927d2fdec6b2ce57867836623af2eba7461b9
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-16 19:32:48 +0300

    Creating Spring MVC app, setting dependencies, creating postgreSQL database

--- src/main/java/com/tpe/WebAppInitializer.java
+++ src/main/java/com/tpe/WebAppInitializer.java
@@ -1,6 +1,24 @@
 package com.tpe;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 // Java based Web Apps configurations need web.xml file
 // this class will be used instead of web.xml
-public class WebAppInitializer {
+
+// AbstractAnnotationConfig: configurations for dispatcherServlet
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
