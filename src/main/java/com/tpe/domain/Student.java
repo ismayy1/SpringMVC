2f71a989d473089a752155782907f16faef94755
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-16 20:20:29 +0300

    Creating Spring MVC app, setting dependencies, creating postgreSQL database

--- src/main/java/com/tpe/domain/Student.java
+++ src/main/java/com/tpe/domain/Student.java
@@ -1,4 +1,64 @@
 package com.tpe.domain;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "t_student")
 public class Student {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int grade;
+
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+//    public void setCreateDate(LocalDateTime createDate) {
+//        this.createDate = createDate;
+//    }
+
 }
