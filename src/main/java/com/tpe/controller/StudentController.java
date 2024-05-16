c8852e862d7fbaea3eeea81a060e10b743acf718
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-16 20:39:39 +0300

    Practice session Day 3, ReoomService Operations

--- src/main/java/com/tpe/controller/StudentController.java
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -1,7 +1,11 @@
 package com.tpe.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // to handle requests
+@RequestMapping ("/students") // http://localhost:8080/springMvc/students  => end point should be directed to this requestMapping
+// we can only use @RequestMapping in class level. if it's used in class level, it's valid for all methods
+// we can only use @RequestMapping in method level, then the path will be valid for method only
 public class StudentController {
 }
