1a102af979799a9be3b157784b03b4d98b621b33
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-19 13:40:06 +0300

    Spring MVC, CRUD Operations

--- src/main/java/com/tpe/repository/StudentRepositoryImplementation.java
+++ src/main/java/com/tpe/repository/StudentRepositoryImplementation.java
@@ -1,14 +1,25 @@
 package com.tpe.repository;
 
 import com.tpe.domain.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
 
 public class StudentRepositoryImplementation implements StudentRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
     @Override
     public List<Student> findAll() {
-        return null;
+        Session session = sessionFactory.openSession();
+
+        String query = "FROM Student";
+        List<Student> allStudents = session.createQuery(query, Student.class).getResultList();
+        session.close();
+        return allStudents;
     }
 
     @Override
