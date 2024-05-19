ba16e4f8b9fc38df4b2e20e6414bcc94709f6cf7
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2024-05-19 13:17:46 +0300

    Spring MVC, CRUD Operations

---
+++ src/main/java/com/tpe/service/StudentServiceImplementation.java
@@ -0,0 +1,27 @@
+package com.tpe.service;
+
+import com.tpe.domain.Student;
+
+import java.util.List;
+
+public class StudentServiceImplementation implements StudentService {
+    @Override
+    public List<Student> listAllStudent() {
+        return null;
+    }
+
+    @Override
+    public void addOrUpdateStudents(Student student) {
+
+    }
+
+    @Override
+    public Student findStudentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+
+    }
+}
