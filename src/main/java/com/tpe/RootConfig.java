65ea1b4880f3149b7658a2fc3513b394eef6a42f
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2025-01-03 21:10:07 +0200

    commit

--- src/main/java/com/tpe/RootConfig.java
+++ src/main/java/com/tpe/RootConfig.java
@@ -1,4 +1,52 @@
 package com.tpe;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:db.properties")
 public class RootConfig {
+
+    @Autowired  // we could use also a constructor injection, and NOT the @Autowired injection
+    private Environment environment;
+
+    private DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("jdbc.driverClassName")));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
+
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+
+        return properties;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        sessionFactory.setDataSource(dataSource()); // when we call this method, we'll get a DataSource obj here
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        sessionFactory.setPackagesToScan(new String[]{"com.tpe.domain"});   // String[] is required in older versions
+
+        return sessionFactory;
+    }
 }
