3dded3773cdd4d84cea96af12e6af97c6280d2df
Ismayyl Atagarryyev <tkm.gala@gmail.com>
2025-01-03 20:24:49 +0200

    commit

--- src/main/java/com/tpe/WebAppInitializer.java
+++ src/main/java/com/tpe/WebAppInitializer.java
@@ -1,4 +1,25 @@
 package com.tpe;
 
-public class WebAppInitializer {
+// We'll use this class as the replacement of web.xml file. A web.xml file is a configuration file for Java based
+// Web Applications
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {
+                RootConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
